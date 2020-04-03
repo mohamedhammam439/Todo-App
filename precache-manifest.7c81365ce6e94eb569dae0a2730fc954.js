78ec1d7ea819bf1f10f8a2d21ef415ccd1c4ff14
mohamedhammam439 <mohamedhammam439@gmail.com>
2020-04-04 01:51:55 +0200

    Updates

---
+++ precache-manifest.7c81365ce6e94eb569dae0a2730fc954.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38d2f8a497f9cdd1ee630082e45f7575",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "bc15a1e40ba2ff9f7d7f",
+    "url": "/Todo-App/static/css/main.28bcddf4.chunk.css"
+  },
+  {
+    "revision": "82d3b2ee1f955207ed6b",
+    "url": "/Todo-App/static/js/2.467e0a19.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Todo-App/static/js/2.467e0a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc15a1e40ba2ff9f7d7f",
+    "url": "/Todo-App/static/js/main.9600c933.chunk.js"
+  },
+  {
+    "revision": "dce626eaf3f0c28c3bd1",
+    "url": "/Todo-App/static/js/runtime-main.2a861cdc.js"
+  }
+]);
